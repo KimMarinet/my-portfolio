259f1d5c0e1e7dd64f4905ed54ae1b7d77ddf31a
KimMarinet <100179052+KimMarinet@users.noreply.github.com>
2025-09-17 23:17:03 +0900

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css';
 import React from 'react';
 import { HelmetProvider } from 'react-helmet-async';
-import { HashRouter, Routes, Route} from 'react-router-dom'
+import { Routes, Route} from 'react-router-dom'
 import ProtfolioPage from './portfolio/pages/ProtfolioPage';
 import MainLayout from './global/layouts/MainLayout';
 import MainPage from './main/pages/MainPage';
@@ -11,17 +11,11 @@ const App = () => {
   return(
     <>
       <HelmetProvider>
-      <HashRouter>
         <Routes>
-
             <Route path='/' element={<MainPage />}></Route>
-
             <Route path="port" element={<ProtfolioPage />}></Route>
-
             <Route path="*" element={<NotFoundPage />}></Route>
-
         </Routes>
-      </HashRouter>
       </HelmetProvider>
     </>
   )
